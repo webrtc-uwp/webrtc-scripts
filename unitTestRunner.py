c993708aee2467fce6ba329100b6cc3fa79a7efc
Sergej Jovanovic <sergej@gnedo.com>
2019-04-18 21:07:36 +0200

    Improved unit tests logger

--- unitTestRunner.py
+++ unitTestRunner.py
@@ -1,6 +1,7 @@
 import os
 import time
 import subprocess
+from datetime import datetime
 
 import config
 from logger import Logger
@@ -30,6 +31,7 @@ class UnitTestRunner:
     start_time = time.time()
     ret = NO_ERROR
     cls.failedTestsCounter = 0
+    cls.totalNumberOfTests = 0
     cls.logger.info('Running unit tests for target: ' + targetName + '; platform: ' + platform + '; cpu: ' + cpu + '; configuration: ' + configuration)
 
     #If path with generated projects is not specified generate path from input arguments
@@ -46,13 +48,24 @@ class UnitTestRunner:
     #Change current working directory to one with generated projects
     Utility.pushd(workingDir)
 
-    cls.unitTestFailuresLog = open('UnitTestFailures.txt','w')
+    #Unit tests summary file
+    summaryFileName = 'UnitTests_' + platform + '_' + cpu + '_' + configuration
+    summaryPath = os.path.join(Settings.userWorkingPath ,summaryFileName + '.txt')
+    if os.path.isfile(summaryPath):
+      summaryFileName += '_' + datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.txt'
+      summaryPath = os.path.join(Settings.userWorkingPath ,summaryFileName)
+    cls.unitTestFailuresLog = open(summaryPath,'w')
 
     for unitTest in Settings.unitTests:
       cls.executeUnitTest(unitTest)
     
+    cls.unitTestFailuresLog.write('***********************************\nTOTAL NUMBER OF TESTS: ' + str(cls.totalNumberOfTests) + '\n')
+    cls.unitTestFailuresLog.write('TOTAL NUMBER OF FAILED TESTS: ' + str(cls.failedTestsCounter) + '\n')
+    
     cls.unitTestFailuresLog.close()
     
+    cls.logger.info('Total number of unit tests is ' + str(cls.totalNumberOfTests))
+
     if cls.failedTestsCounter > 0:
       if cls.failedTestsCounter == 1:
         cls.logger.warning(str(cls.failedTestsCounter) + ' unit test has failed. You can check details in file ' + os.path.abspath(cls.unitTestFailuresLog.name))
@@ -107,23 +120,41 @@ class UnitTestRunner:
   @classmethod
   def parseResults(cls, unitTestName, outputFile):
 
+    ret = True
+    unitTestFailures = 0
+    numberOfUnitTests = 0
     with open(outputFile, 'r') as fileToParse:
       fileContent = fileToParse.read()
 
     unitTests = fileContent.split(config.UNIT_TESTS_LOG_SEPARATOR)
 
+    cls.unitTestFailuresLog.write(unitTestName)
+    cls.unitTestFailuresLog.write('\n========================\n')
+
     for unitTest in unitTests:
       testResults = unitTest.split(config.UNIT_TEST_RESULTS_SEPARATOR)
       testResult = testResults[-1]
-      if '[  FAILED  ]' in testResult:
-        cls.unitTestFailuresLog.write(unitTestName)
-        cls.unitTestFailuresLog.write('\n==============\n')
-        for line in testResult.split('\n'):
-          if '[  FAILED  ]' in line and 'listed below' not in line:
-            cls.failedTestsCounter += 1
-            cls.unitTestFailuresLog.write(line + '\n')
-        cls.unitTestFailuresLog.write('==============\n\n')
-        cls.unitTestFailuresLog.flush()
+      
+      for line in testResult.split('\n'):
+        if '[==========] ' in line:
+          listForNumberOfTests = line.split(' ')
+          if len(listForNumberOfTests) > 1:
+            numberOfTests = listForNumberOfTests[1]
+            numberOfUnitTests += int(numberOfTests)
+        if '[  FAILED  ]' in line and 'listed below' not in line:
+          unitTestFailures += 1
+          cls.unitTestFailuresLog.write(line + '\n')
+
+    cls.totalNumberOfTests += numberOfUnitTests
+    cls.failedTestsCounter += unitTestFailures
+    
+    if unitTestFailures > 0:
+      cls.unitTestFailuresLog.write('-----------------------------\n')
+    cls.unitTestFailuresLog.write('Total number of tests: ' + str(numberOfUnitTests) + '\n') 
+    cls.unitTestFailuresLog.write('Total number of failed tests: ' + str(unitTestFailures) + '\n')
+    cls.unitTestFailuresLog.write('========================\n\n\n\n')
+    cls.unitTestFailuresLog.flush()
+    
     return True
 
   @classmethod
